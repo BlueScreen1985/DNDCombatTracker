54d74df3cdccb7dfe2b5d6b79f9fcef6a9723f1c
BlueScreen <teo.fum@gmail.com>
2018-09-11 09:58:20 -0300

    Improved text field focus

--- DNDCombatTracker/App.xaml.cs
+++ DNDCombatTracker/App.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DNDCombatTracker
 {
@@ -13,5 +9,29 @@ namespace DNDCombatTracker
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.PreviewMouseLeftButtonDownEvent,
+                new MouseButtonEventHandler(SelectivelyHandleMouseButton), true);
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.GotKeyboardFocusEvent,
+                new RoutedEventHandler(SelectAllText), true);
+
+            base.OnStartup(e);
+        }
+
+        private static void SelectivelyHandleMouseButton(object sender, MouseButtonEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && !textBox.IsKeyboardFocusWithin)
+            {
+                if (e.OriginalSource.GetType().Name == "TextBoxView")
+                {
+                    e.Handled = true;
+                    textBox.Focus();
+                }
+            }
+        }
+
+        private static void SelectAllText(object sender, RoutedEventArgs e) => (sender as TextBox).SelectAll();
     }
 }

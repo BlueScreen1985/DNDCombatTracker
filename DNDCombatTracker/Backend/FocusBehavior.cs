1b4c020f6d92f4d7dd765a944f16a51e35768df3
BlueScreen <teo.fum@gmail.com>
2018-09-12 07:38:20 -0300

    Fixed tab order in dialogs, added autofocus

---
+++ DNDCombatTracker/Backend/FocusBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DNDCombatTracker.Behaviors
+{
+    // Copy-pasted code from stack overflow, I /think/ I know how it works
+    public static class FocusBehavior
+    {
+        public static readonly DependencyProperty FocusFirstProperty =
+            DependencyProperty.RegisterAttached("FocusFirst", typeof(bool), typeof(FocusBehavior),
+                new PropertyMetadata(false, OnFocusFirstPropertyChanged));
+
+        public static bool GetFocusFirst(Control control) => (bool)control.GetValue(FocusFirstProperty);
+        public static void SetFocusFirst(Control control, bool value) => control.SetValue(FocusFirstProperty, value);
+
+        static void OnFocusFirstPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            Control control = obj as Control;
+            if (control == null || !(args.NewValue is bool))
+                return;
+
+            if ((bool)args.NewValue)
+            {
+                control.Loaded += (sender, e) =>
+                    control.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+            }
+        }
+    }
+}
